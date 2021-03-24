45c175e982f1693f96749aa5c12de61f911d3a65
sherin119 <sherin119@gmail.com>
2021-03-24 16:23:59 +0530

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 50.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
---
+++ lib/location.dart
@@ -0,0 +1,25 @@
+
+import 'package:geolocator/geolocator.dart';
+
+
+class Location {
+
+  Location({this.lat, this.long});
+  double lat;
+  double long;
+
+  Future <void> getCurrentLocation() async {
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      lat = position.latitude;
+      long = position.longitude;
+    }
+
+    catch (e){
+      print(e);
+    }
+
+  }
+
+}
